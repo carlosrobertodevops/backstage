22cf883385e8c42eac4695cb0ebcce9a6e12e28a
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-01-02 16:17:03 -0300

    Ajustes de barra

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -76,10 +76,9 @@ export const Root = ({ children }: PropsWithChildren<{}>) => (
         />
         <SidebarItem icon={ExtensionIcon} to="api-docs" text="APIs" />
         <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-        <SidebarItem icon={CreateIcon} to="catalog-import" text="Register..." />
-        <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." />
+        <SidebarItem icon={CreateIcon} to="catalog-import" text="Create..." />
+        <SidebarItem icon={CreateComponentIcon} to="create" text="Register..." />
          {/* <SidebarItem icon={CreateComponentIcon} to="create" text="Create..." /> */}
-  
         {/* End global nav */}
         <SidebarDivider />
         <SidebarScrollWrapper>
