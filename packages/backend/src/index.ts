805253a110e3b573ce18c71e06086ea18ae6bb2c
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-01-10 20:36:06 -0300

    Ajustes

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -62,7 +62,7 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 backend.add(import('@backstage-community/plugin-copilot-backend'));
 backend.add(import('@backstage/plugin-devtools-backend'));
-//
+
 // backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
 
 const customAuthResolver = createBackendModule({
