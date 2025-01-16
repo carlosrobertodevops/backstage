b7c8a10cc2339a10af81c4573b13100e32d730d3
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-01-16 09:58:07 -0300

    Ajustes

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -18,23 +18,22 @@ import {
 
 export const apis: AnyApiFactory[] = [
   createApiFactory({
-    api: graphQlBrowseApiRef,
-    deps: { errorApi: errorApiRef, githubAuthApi: githubAuthApiRef },
-    factory: ({ errorApi, githubAuthApi }) =>
-        GraphQLEndpoints.github({
-          id: 'github',
-          title: 'GitHub',
-          url: 'https://api.github.com/graphql',
-          errorApi: errorApi,
-          githubAuthApi: githubAuthApi
-        })
-      ])
-  }),
-
-  createApiFactory({
     api: scmIntegrationsApiRef,
     deps: { configApi: configApiRef },
     factory: ({ configApi }) => ScmIntegrationsApi.fromConfig(configApi),
   }),
   ScmAuth.createDefaultApiFactory(),
+
+  createApiFactory({
+    api: graphQlBrowseApiRef,
+    deps: { errorApi: errorApiRef, githubAuthApi: githubAuthApiRef },
+    factory: ({ errorApi, githubAuthApi }) =>
+      GraphQLEndpoints.github({
+        id: 'github',
+        title: 'GitHub',
+        url: 'https://api.github.com/graphql',
+        errorApi: errorApi,
+        githubAuthApi: githubAuthApi,
+      }),
+  }),
 ];
