74123dd371ae8f8ba585ec158338102ae2cc3122
Carlos Roberto <carlos.roberto.devops@gmail.com>
2025-01-16 15:11:28 -0300

    Ajustes

--- packages/app/src/apis/graphiqlApis.ts
+++ packages/app/src/apis/graphiqlApis.ts
@@ -1,26 +1,46 @@
 // packages/app/src/apis.ts
 import {
   AnyApiFactory,
-  createApiFactory
+  createApiFactory,
+  errorApiRef,
+  githubAuthApiRef
 } from '@backstage/core';
 import {
   graphQlBrowseApiRef,
   GraphQLEndpoints
 } from '@backstage/plugin-graphiql'
 
-export const graphiqlApis: AnyApiFactory[] = [
+export const apis: AnyApiFactory[] = [
   createApiFactory({
     api: graphQlBrowseApiRef,
-    deps: { },
-    factory: () =>
-      GraphQLEndpoints.from([
-        // use the create function to connect an unauthenticated GraphQL API
-        GraphQLEndpoints.create({
-          id: 'gitlab',
-          title: 'GitLab',
-          url: 'https://gitlab.com/api/graphql',
-          headers: { 'Some-Key': 'Some-Value' }
+    deps: { errorApi: errorApiRef, githubAuthApi: githubAuthApiRef },
+    factory: ({ errorApi, githubAuthApi }) =>
+        ({
+          ...GraphQLEndpoints.github({
+            id: 'github',
+            title: 'GitHub',
+            url: 'https://api.github.com/graphql',
+            errorApi: errorApi,
+            githubAuthApi: githubAuthApi
+          }),
+          getEndpoints: async () => [
+            {
+              id: 'github',
+              title: 'GitHub',
+              url: 'https://api.github.com/graphql',
+              fetcher: async (body: any) => {
+                const response = await fetch('https://api.github.com/graphql', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${await githubAuthApi.getAccessToken()}`,
+                  },
+                  body: JSON.stringify(body),
+                });
+                return response.json();
+              }
+            }
+          ]
         })
-      ])
   })
 ];
